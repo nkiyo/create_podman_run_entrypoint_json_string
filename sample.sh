0b2bbc4a42033ef55f82cf22d11654d891248a0a
nkiyo <nakahara.kiyoshi@gmail.com>
2024-08-15 12:18:10 +0900

    up

--- sample.sh
+++ sample.sh
@@ -6,7 +6,12 @@ source sample.env
 arg_cnt=1
 jq_arg=()
 for var in ${!PARAM_@}; do
+  # パラメータ名を変換する
+  # 例: PARAM_HOGE_AAA => --hoge-aaa 
   key=$(echo --$var | tr '[:upper:]' '[:lower:]' | sed 's/param_//g' | tr '_' '-')
+
+  # パラメータ名と値を--argで渡すためのリストを作る
+  # 例: --arg i1 --hoge-aaa --arg i2  abc
   jq_arg+=("--arg")
   jq_arg+=(i$arg_cnt)
   jq_arg+=("$key")
@@ -15,6 +20,9 @@ for var in ${!PARAM_@}; do
   jq_arg+=("${!var}")
   arg_cnt=$((arg_cnt + 2))
 done
+
+# jqのフォーマット文字列の部分
+# 例: '[$i1, $i2, $i3]'
 jq_arg_list=$(seq $((arg_cnt - 1)) | xargs -I{} echo '$i{}' | paste -sd, | sed 's/^/[/; s/$/]/')
 
 #echo jq -nc "${jq_arg[@]}" $jq_arg_list
